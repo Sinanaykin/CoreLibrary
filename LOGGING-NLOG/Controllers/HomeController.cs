aa4a988b630b12a694d3fcbadd6296521e91e771
AD\sinan.aykin <sinan.aykin@gedik.com>
2024-03-11 00:51:01 +0300

    loglama ve nlog kütüphanesi eklendi.

---
+++ LOGGING-NLOG/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using LOGGING_NLOG.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LOGGING_NLOG.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("INDEX SAYFASI BAŞLAMIŞTIR.......");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
