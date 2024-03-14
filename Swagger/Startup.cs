e2eaa1a9d896b5f1b8dcc98f5e863cb613e3ec78
AD\sinan.aykin <sinan.aykin@gedik.com>
2024-03-15 00:41:25 +0300

    swagger entegrasyonu bitti

---
+++ Swagger/Startup.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.AspNetCore.Swagger;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Swagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new Info
+                {
+                    Version = "v1",
+                    Title = "Swagger Api",
+                    Description = "Sinan ulaþýn"
+
+
+                });
+
+                var xmlFile = $"{ Assembly.GetExecutingAssembly().GetName().Name}.xml";//dll de üretilen xlm in dosyanýn ismini aldýk
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);//yorumlarý alacaðý yerin pathini alýyoruz
+                c.IncludeXmlComments(xmlPath);//yorumlarý alacaðý yerin pathini veriyoruz
+            });
+
+           
+     
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/V1/swagger.json", "My Swagger");
+            });
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
